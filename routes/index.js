d7638d5bce61070de3f97bda33afdc102e404294
oswald <oswald.hou@sap.com>
2017-09-18 14:41:37 +0800

    add physical

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,27 @@ router.get('/', function(req, res, next) {
 	res.render('index');
 });
 
+router.get('/physical', function(req, res, next) {
+	var uid = req.session.uid;
+	var type = 'physical';
+
+	req.getConnection(function(err, conn) {
+        if (err) {
+            return next(err);
+        } else {
+            conn.query('select * from task where task_type=?', [type], function(err,result) {
+                if (err) {
+                    return next(err);
+                } else {
+					res.render('index_physical',{
+							tasks: result
+						});
+                }
+            });
+        }
+    });
+
+	// res.render('index_physical');
+});
+
 module.exports = router;
--- routes/task.js
+++ routes/task.js
@@ -3,8 +3,31 @@ var router = express.Router();
 
 /* GET task page. */
 router.get('/list', function(req, res, next) {
-	console.log(req.session.uid);
-	res.render('index');
+
+	var uid = req.session.uid;
+
+	req.getConnection(function(err, conn) {
+        if (err) {
+            return next(err);
+        } else {
+            //conn.query('select * from task where user_id =?', [uid], function(err,result) {
+            conn.query('select * from user', [], function(err,result) {
+                if (err) {
+                    return next(err);
+                } else {
+                	// if(result.length) {
+                	console.log(result.length);
+					res.render('index',{
+							users: result
+						});
+                	// }
+                }
+            });
+        }
+    });
+	// res.render('index',{
+	// 	message: 'user'
+	// });
 });
 
 module.exports = router;
